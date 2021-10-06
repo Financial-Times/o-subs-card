658e4b9ab12a71b68b6c3ef0a0f6f24a118d90a6
chee <chee@snoot.club>
2021-10-06 18:01:30 +0100

    Add note about The Origami Component System

--- demos/src/demo.js
+++
@@ -1,9 +0,0 @@
-import { SubsCard } from './../../main.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-	const cards = SubsCard.init();
-
-	document.querySelector('.toggle-all').addEventListener('click', () => {
-		cards.forEach(card => card.toggle());
-	});
-});
